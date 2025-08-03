ca1d83e56d2eec308cd152fb5766bb47ef851ea5
DimitriGranger <dimitri.granger@akur8.com>
2025-08-03 19:29:15 +0900

    wip

---
+++ build.sh
@@ -0,0 +1,2 @@
+python3 src/main.py "/statichtml/"
+cd docs && python3 -m http.server 8888
--- src/generate_file_utils.py
+++ src/generate_file_utils.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
             return x[2:].strip()
     raise Exception
 
-def generate_file(from_path, template_path, dest_path):
+def generate_file(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as md, open(template_path, "r") as template, open(dest_path, "w") as destination:
@@ -20,9 +20,11 @@ def generate_file(from_path, template_path, dest_path):
         title = extract_title(md_string)
         result = template.read().replace("{{ Title }}", title)
         result = result.replace("{{ Content }}", html_string)
+        result = result.replace('href="/', f'href="{base_path}')
+        result = result.replace('src="/', f'src="{base_path}')
         destination.write(result)
 
-def generate_files_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_files_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     all_items = os.listdir(dir_path_content)
 
     for x in all_items:
@@ -31,9 +33,9 @@ def generate_files_recursive(dir_path_content, template_path, dest_dir_path):
 
         if os.path.isfile(source_path):
             write_path = destination_path[:-3] + ".html"
-            generate_file(source_path, template_path, write_path)
+            generate_file(source_path, template_path, write_path, base_path)
         if os.path.isdir(source_path):
             os.mkdir(destination_path)
-            generate_files_recursive(source_path, template_path, destination_path)
+            generate_files_recursive(source_path, template_path, destination_path, base_path)
 
 
